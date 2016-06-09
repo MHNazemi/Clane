b0201e4f0cccfb2a499759029feeab2ff5a33cda
Mohammad H. Nazemi <nazemimohammadh@gmail.com>
2016-06-09 18:20:16 +0430

    FirstCommit
    Start state of Project

---
+++ Clane/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clane
+{
+    class Program
+    {
+        private SQLiteConnection sql_con;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!\nFor fetching news enter 'f', \nfor continue classification enter 'c' \nand for exit enter 'e'\n:");
+            string input = "";
+            var mainLoop = true;
+            while (mainLoop)
+            {
+                input = "";
+                try
+                {
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "f":
+                        case "F":
+
+                            break;
+                        case "c":
+                        case "C":
+                            break;
+                        case "e":
+                        case "E":
+                            mainLoop = false;
+                            break;
+                        default:
+                            Console.WriteLine("\nYour input was incorrect! \nFor fetching news enter 'f', \nfor continue classification enter 'c' \nand for exit enter 'e'\n:");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nYour input was incorrect! \nFor fetching news enter 'f', \nfor continue classification enter 'c' \nand for exit enter 'e'\n:");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    static class Crawler
+    {
+        public static void test()
+        {
+
+        }
+    }
+    static class Classifier
+    {
+
+    }
+
+}
